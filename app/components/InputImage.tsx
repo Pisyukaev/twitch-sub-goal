34d84c0d28019a6688362a88e464b0501b157c1d
Vitaly Zakutny <stalker-52@mail.ru>
2023-03-02 00:23:58 +0300

    chore: added regexp to delete "url()" wrapper from img src

--- app/components/InputImage.tsx
+++ app/components/InputImage.tsx
@@ -6,9 +6,11 @@ const InputImage = ({ onChange, imgData }) => {
   const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) =>
     onChange("background-image", `url(${value})`)
 
+  const src = imgData["background-image"].replaceAll(/url\(|\)/g, "")
+
   return (
     <Input.Wrapper label="Image link">
-      <Input value={imgData["background-image"]} onChange={handleChange} />
+      <Input value={src} onChange={handleChange} />
     </Input.Wrapper>
   )
 }
