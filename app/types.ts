cdcca9a96ce1e50a9d72a46fbd971ce52359bacb
Vitaly Zakutny <stalker-52@mail.ru>
2023-03-31 22:56:40 +0300

    chore: upd types

--- app/types.ts
+++ app/types.ts
@@ -1,10 +1,24 @@
 export interface SelectorProps {
-  value: string
   label: string
-  color: string
-  property: "color" | "background-color"
+  value: string
+  property:
+    | "color"
+    | "background-color"
+    | "border-color"
+    | "border-width"
+    | "border-radius"
+    | "content"
+  selector: string
+  group?: string
+  componentName: string
 }
 
 export interface StylesData {
   [key: string]: { [prop: string]: string }
 }
+
+export type UpdateStylesFn = (
+  selector: string,
+  prop: string,
+  saveValue: string
+) => void
