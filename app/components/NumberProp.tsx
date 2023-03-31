02bd05a7ea33dfec9f4b6ac3e7f20c55404df822
Vitaly Zakutny <stalker-52@mail.ru>
2023-03-31 22:59:02 +0300

    chore: added a component for set number of style

---
+++ app/components/NumberProp.tsx
@@ -0,0 +1,36 @@
+import { NumberInput } from "@mantine/core"
+import React, { useState } from "react"
+
+import type { SelectorProps, UpdateStylesFn } from "~app/types"
+
+import { getMeasureValue } from "./utils"
+
+interface Props {
+  selectedStyles: SelectorProps
+  onUpdate: UpdateStylesFn
+}
+
+function NumberProp({ selectedStyles, onUpdate }: Props) {
+  const { value, label, property, selector } = selectedStyles
+  const { numberValue, measureValue } = getMeasureValue(value)
+  const [number, setNumber] = useState(numberValue)
+
+  const handleChange = (newNumber: number) => {
+    onUpdate(selector, property, `${newNumber}${measureValue}`)
+
+    setNumber(newNumber)
+  }
+
+  return (
+    <NumberInput
+      defaultValue={number}
+      value={number}
+      placeholder={label}
+      label={label}
+      onChange={handleChange}
+      min={0}
+    />
+  )
+}
+
+export default NumberProp
