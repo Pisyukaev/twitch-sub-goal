d66987f2ec0d846f2388ce99891f49a2eed1660d
Vitaly Zakutny <stalker-52@mail.ru>
2023-04-19 00:35:12 +0300

    chore: added function to request fonts and data of fonts

---
+++ app/api/fonts.ts
@@ -0,0 +1,57 @@
+import { prepareFont } from "~app/components/utils"
+import type { Font, FontData, FontList } from "~app/types"
+
+const FONTS_SOURCE_API = "https://api.fontsource.org/v1/fonts"
+const SUBSETS = ["latin", "latin-ext", "cyrillic", "cyrillic-ext"]
+const WEIGHTS = ["400"]
+
+export async function fetchFontData(fontFamily: string) {
+  let fontData = {} as FontData
+  try {
+    const response = await fetch(`${FONTS_SOURCE_API}/${fontFamily}`)
+    fontData = await response.json()
+  } catch (e) {
+    console.error("Error fetching font", e)
+  } finally {
+    return fontData
+  }
+}
+
+export async function loadFont(fontFamily: string) {
+  for (const fontFace of document.fonts.values()) {
+    if (fontFace.family === fontFamily) {
+      return
+    }
+  }
+
+  const fontData = await fetchFontData(fontFamily)
+  const fontFaces = prepareFont(fontData)
+
+  try {
+    await Promise.all(fontFaces.map((fontFace) => fontFace.load()))
+  } catch (e) {
+    console.error("Error loading fonts in document", e)
+  }
+}
+
+export async function fetchFontList() {
+  let fonts = [] as Font[]
+
+  try {
+    const response = await fetch(
+      `${FONTS_SOURCE_API}?subsets=${SUBSETS.join(",")}&weights=${WEIGHTS.join(
+        ","
+      )}`
+    )
+    const list: FontList = await response.json()
+
+    fonts = list.map(({ id, family }) => ({
+      value: id,
+      label: family
+    }))
+  } catch (e) {
+    console.warn("Error fetching fonts", e)
+  } finally {
+    return fonts
+  }
+}
