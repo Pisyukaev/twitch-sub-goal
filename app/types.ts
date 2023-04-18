6c8b4bb52044b2c2ed396c3f86bab2b8ac41609a
Vitaly Zakutny <stalker-52@mail.ru>
2023-04-19 00:33:53 +0300

    chore: added types for fonts

--- app/types.ts
+++ app/types.ts
@@ -8,6 +8,8 @@ export interface SelectorProps {
     | "border-width"
     | "border-radius"
     | "content"
+    | "font-size"
+    | "font-family"
   selector: string
   group?: string
   componentName: string
@@ -22,3 +24,62 @@ export type UpdateStylesFn = (
   prop: string,
   saveValue: string
 ) => void
+
+export interface Font {
+  value: string
+  label: string
+}
+
+export type FontStyle = "normal" | "italic"
+
+export type FontVariants = "400" | "500" | "800"
+
+export type FontSubset = "latin" | "latin-ext" | "cyrillic" | "cyrillic-ext"
+
+export type FontType = "ttf" | "woff" | "woff2"
+
+export interface FontItem {
+  category: string
+  defSubset: string
+  family: string
+  id: string
+  lastModified: string
+  styles: FontStyle[]
+  subsets: FontSubset[]
+  type: string
+  variable: boolean
+  version: string
+  weights: number[]
+}
+
+export type FontList = FontItem[]
+
+export type FontVariant = {
+  [key in FontVariants]: {
+    [key in FontStyle]: {
+      [key in FontSubset]: {
+        url: {
+          [key in FontType]: string
+        }
+      }
+    }
+  }
+}
+
+export interface FontData {
+  id: string
+  family: string
+  subsets: FontSubset
+  weights: number[]
+  styles: string[]
+  unicodeRange: {
+    [key: string]: string
+  }
+  defSubset: string
+  variable: boolean
+  lastModified: string // like a "2020-09-10"
+  category: string
+  version: string
+  type: string
+  variants: FontVariant
+}
