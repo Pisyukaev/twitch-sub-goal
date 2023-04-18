c752a2712719698370aefe5a67d24a46bd55fd8b
Vitaly Zakutny <stalker-52@mail.ru>
2023-04-19 00:34:28 +0300

    chore: added utils to work with fonts

--- app/components/utils.ts
+++ app/components/utils.ts
@@ -1,3 +1,5 @@
+import type { FontData } from "~app/types"
+
 export const getMeasureValue = (string: string) => {
   const regExp = /((?:\d*\.)?\d+)(\D+)/
   const [, numberValue, measureValue] = string.match(regExp)
@@ -6,4 +8,86 @@ export const getMeasureValue = (string: string) => {
     numberValue: Number(numberValue) || 0,
     measureValue: measureValue || ""
   }
+}
+
+const createFontFace = (fontFamily: string, url: string) => {
+  const fontFace = new FontFace(fontFamily, `url(${url})`)
+  document.fonts.add(fontFace)
+
+  return fontFace
+}
+
+export const getFontStyle = (fontVariant) => {
+  const style = Object.keys(fontVariant).includes("normal")
+    ? "normal"
+    : "italic"
+
+  return style
+}
+
+export const getFontSubsetWithUrls = (fontData: FontData) => {
+  const fontVariant = fontData.variants["400"]
+  const style = getFontStyle(fontVariant)
+  const subsets = Object.entries(fontVariant[style])
+
+  return subsets
+}
+
+/**
+ * Creates FontFace objects
+ * @param font - Font data from Fontsource API
+ * @returns Array of FontFace objects
+ */
+export const prepareFont = (font: FontData) => {
+  const subsets = getFontSubsetWithUrls(font)
+  const fontFaces = subsets.map(
+    ([
+      subset,
+      {
+        url: { woff2 }
+      }
+    ]) => createFontFace(font.family, woff2)
+  )
+
+  return fontFaces
+}
+
+/**
+ * Creates CSS strings for @font-face rules
+ * @param fontData - Font data from Fontsource API
+ * @returns Array of CSS strings
+ * @example createFontFacesCSS(fontData)
+ * // [
+ * //   "@font-face {
+ * //     font-family: 'Inter';
+ * //     font-style: normal;
+ * //     font-weight: 400;
+ * //     src: url(https://fonts.gstatic.com/../qwer.woff2) format('woff2');
+ * //     unicode-range: U+0000-00FF, U+0131, ...;
+ * //   }",
+ * // ]
+ *
+ */
+export const createFontFacesCSS = (fontData: FontData): string[] => {
+  const fontVariant = fontData.variants["400"]
+  const style = getFontStyle(fontVariant)
+  const subsets = getFontSubsetWithUrls(fontData)
+
+  const fontFaces = subsets.map(
+    ([
+      subset,
+      {
+        url: { woff2 }
+      }
+    ]) => `@font-face {
+      font-family: '${fontData.family}';
+      font-style: ${style};
+      font-weight: 400;
+      src: url(${woff2}) format('woff2');
+      unicode-range: ${fontData.unicodeRange[subset]};
+    }
+    `
+  )
+
+  return fontFaces
 }
